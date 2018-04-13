99094367657f96670247681e8c6bdbbfe2f88e0a
foi <foi.first@gmail.com>
2018-04-13 21:07:31 +0700

    add args

---
+++ README.md
@@ -0,0 +1,9 @@
+# go-socks5-server
+
+## how to install
+```
+git clone https://github.com/foi/go-socks5-server
+cd go-socks5-server
+go get ./...
+go build server.go
+```
--- go-socks5-server.go
+++ go-socks5-server.go
@@ -1,27 +1,37 @@
 package main
 
 import socks5 "github.com/armon/go-socks5"
+import "fmt"
+import "flag"
 
 func main(){
+  ip := flag.String("ip", "0.0.0.0", "")
+  port := flag.String("port", "1080", "")
+  user := flag.String("user", "changeme", "username")
+  pass := flag.String("pass", "andme", "password")
+
+  flag.Parse()
+
   creds := socks5.StaticCredentials{
-    "user": "pass",
+    *user: *pass,
   }
 
   cator := socks5.UserPassAuthenticator{Credentials: creds}
-  
+
   conf := &socks5.Config{
     AuthMethods: []socks5.Authenticator{cator},
   }
 
   server, err := socks5.New(conf)
 
+  fmt.Println("go-socks5-server started successfully.")
+
   if err != nil {
     panic(err)
   }
 
-  if err := server.ListenAndServe("tcp", "0.0.0.0:1080");
+  if err := server.ListenAndServe("tcp", fmt.Sprintf("%s:%s", *ip, *port));
   err != nil {
     panic(err)
   }
 }
-
