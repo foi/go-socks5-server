f8b94e5b4db920dea41e5a5b74968002d0a8e954
foi <foi.first@gmail.com>
2018-04-15 09:08:58 +0700

    got fmt

--- go-socks5-server.go
+++ go-socks5-server.go
@@ -2,54 +2,53 @@ package main
 
 import socks5 "github.com/armon/go-socks5"
 import (
-  "fmt"
-  "encoding/json"
-  "os"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
 type Config struct {
-    Ip string
-    Port string
-    Credentials []Credentials
+	Ip          string
+	Port        string
+	Credentials []Credentials
 }
 
 type Credentials struct {
-  User string
-  Pass string
+	User string
+	Pass string
 }
 
-func main(){
-  file, _ := os.Open("/etc/go-socks5-server.config.json")
-  defer file.Close()
-  decoder := json.NewDecoder(file)
-  configuration := Config{}
-  err := decoder.Decode(&configuration)
-  if err != nil {
-    panic(err)
-  }
+func main() {
+	file, _ := os.Open("/etc/go-socks5-server.config.json")
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Config{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		panic(err)
+	}
 
-  creds := socks5.StaticCredentials{}
+	creds := socks5.StaticCredentials{}
 
-  for _, c := range configuration.Credentials {
-    creds[c.User] = c.Pass
-  }
+	for _, c := range configuration.Credentials {
+		creds[c.User] = c.Pass
+	}
 
-  cator := socks5.UserPassAuthenticator{Credentials: creds}
+	cator := socks5.UserPassAuthenticator{Credentials: creds}
 
-  conf := &socks5.Config{
-    AuthMethods: []socks5.Authenticator{cator},
-  }
+	conf := &socks5.Config{
+		AuthMethods: []socks5.Authenticator{cator},
+	}
 
-  server, err := socks5.New(conf)
+	server, err := socks5.New(conf)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  fmt.Println(fmt.Sprintf("go-socks5-server started successfully: ip %s port %s", configuration.Ip, configuration.Port))
+	fmt.Println(fmt.Sprintf("go-socks5-server started successfully: ip %s port %s", configuration.Ip, configuration.Port))
 
-  if err := server.ListenAndServe("tcp", fmt.Sprintf("%s:%s", configuration.Ip, configuration.Port));
-  err != nil {
-    panic(err)
-  }
+	if err := server.ListenAndServe("tcp", fmt.Sprintf("%s:%s", configuration.Ip, configuration.Port)); err != nil {
+		panic(err)
+	}
 }
